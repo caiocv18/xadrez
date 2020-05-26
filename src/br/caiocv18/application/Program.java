8271cc5de5920ba31c7c068cf62edeb99ed047ab
caiocv18 <caioviniciuscv18@gmail.com>
2020-05-26 19:16:39 -0300

    criando a classe Position

---
+++ src/br/caiocv18/application/Program.java
@@ -0,0 +1,14 @@
+package br.caiocv18.application;
+
+import br.caiocv18.boardgame.Position;
+
+/**
+ * Program
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
