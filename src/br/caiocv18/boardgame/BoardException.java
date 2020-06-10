20a354a30bb536de15321192b4b8c5788b6db4c4
caiocv18 <caioviniciuscv18@gmail.com>
2020-06-10 14:35:09 -0300

    adicionando e implementando a BoardException

---
+++ src/br/caiocv18/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package br.caiocv18.boardgame;
+
+public class BoardException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+    
+}
