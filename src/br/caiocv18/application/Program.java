48ed498d04ac8a64f837e53b1b0022961bb0ac2f
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-05-20 16:56:03 -0300

    Clear screen and exceptions

--- src/br/caiocv18/application/Program.java
+++ src/br/caiocv18/application/Program.java
@@ -1,7 +1,9 @@
 package br.caiocv18.application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import br.caiocv18.chess.ChessException;
 import br.caiocv18.chess.ChessMatch;
 import br.caiocv18.chess.ChessPiece;
 import br.caiocv18.chess.ChessPosition;
@@ -11,12 +13,12 @@ import br.caiocv18.chess.ChessPosition;
  */
 public class Program {
 
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        ChessMatch chessMatch = new ChessMatch();
-        
-        while (true) {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+
+		try {
 			UI.printBoard(chessMatch.getPieces());
 			System.out.println();
 			System.out.print("Source: ");
@@ -27,6 +29,12 @@ public class Program {
 			ChessPosition target = UI.readChessPosition(sc);
 
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		} catch (ChessException e) {
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		} catch (InputMismatchException e) {
+			System.out.println(e.getMessage());
+			sc.nextLine();
 		}
-    }
+	}
 }
