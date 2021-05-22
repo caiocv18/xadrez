39b05819292e46bdd1572e477c2e26810ac53579
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-05-21 23:06:45 -0300

    Possible moves of a Rook pt3

--- src/br/caiocv18/chess/ChessPiece.java
+++ src/br/caiocv18/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package br.caiocv18.chess;
 
 import br.caiocv18.boardgame.Board;
 import br.caiocv18.boardgame.Piece;
+import br.caiocv18.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -15,4 +16,9 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
+
+    protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
