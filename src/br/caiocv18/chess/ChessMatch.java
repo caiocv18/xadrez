16e4df5c61dd6fa147e6e53ba1e563495d06cf71
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-05-23 19:31:19 -0300

    Bishop

--- src/br/caiocv18/chess/ChessMatch.java
+++ src/br/caiocv18/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import br.caiocv18.boardgame.Board;
 import br.caiocv18.boardgame.Piece;
 import br.caiocv18.boardgame.Position;
+import br.caiocv18.chess.pieces.Bishop;
 import br.caiocv18.chess.pieces.King;
 import br.caiocv18.chess.pieces.Pawn;
 import br.caiocv18.chess.pieces.Rook;
@@ -193,7 +194,9 @@ public class ChessMatch {
 
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -204,8 +207,10 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
-		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
