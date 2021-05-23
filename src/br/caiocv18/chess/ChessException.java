08a7364f83626779566b53aeca82c6e2c4b4663c
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-05-23 20:02:06 -0300

    Fix - better treatment to promotion

--- src/br/caiocv18/chess/ChessException.java
+++ src/br/caiocv18/chess/ChessException.java
@@ -3,8 +3,6 @@ package br.caiocv18.chess;
 import br.caiocv18.boardgame.BoardException;
 
 public class ChessException extends BoardException{
-    private static final long serialVersionID = 1L;
-
     public ChessException(String msg){
         super(msg);
     }
